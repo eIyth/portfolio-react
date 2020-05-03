f9cd4631ead0792f76e8555f3579c97602b5d5f0
eIyth <gkerno@gmail.com>
2020-05-03 23:23:46 +0200

    Navbar fixes 1

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,29 +4,28 @@ class Menu extends React.Component{
     render() {
       return(
 
-    <div className="absolute  top-0 w-screen">
-    <header class="lg:px-16 px-6 flex  flex-wrap items-center lg:py-0 py-2 bg-transparent">
-        <div class="flex-1 flex justify-between items-center">
+    <div className="absolute top-0 w-screen">
+    <header className="lg:px-16 px-6 flex  flex-wrap items-center lg:py-0 py-2 bg-transparent">
+        <div className="flex-1 flex justify-between items-center">
             <a href="/">
-            <img class="w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="https://image.flaticon.com/icons/svg/243/243270.svg" alt="Home"/>
+            <img className="w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="https://image.flaticon.com/icons/svg/243/243270.svg" alt="Home"/>
             </a>
         </div>
 
-    <label for="menu-toggle" class="pointer-cursor lg:hidden block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
-    <input class="hidden" type="checkbox" id="menu-toggle" />
+    <label for="menu-toggle" className="lg:hidden cursor-pointer block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
+    <input className="hidden" type="checkbox" id="menu-toggle" />
 
-    <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+    <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
         <nav>
-        <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-            <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="/Home">Home</a></li>
-            <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="/Projects">Projects</a></li>
-            <li><a class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="/Contact">Contact</a></li>
+        <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+            <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="/Home">Home</a></li>
+            <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="/Projects">Projects</a></li>
+            <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="/Contact">Contact</a></li>
         </ul>
         </nav>
-        <a href="/" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
-        <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="https://pbs.twimg.com/profile_images/1164206749093695488/e_wm8Yhc_400x400.jpg" alt="Gwenchlan Le Kerneau"/>
+        <a href="/" className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+        <img className="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="https://pbs.twimg.com/profile_images/1164206749093695488/e_wm8Yhc_400x400.jpg" alt="Gwenchlan Le Kerneau"/>
         </a>
-
     </div>
     </header>
     </div>
