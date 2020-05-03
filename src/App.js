0e17ca98013bd8daddc23db726cffda59aa21357
eIyth <gkerno@gmail.com>
2020-05-03 23:09:46 +0200

    Changing page name

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Menu from './components/menu.js';
 import AboutMe from './components/about.js';
 import Projects from './components/projects';
+import { Helmet } from 'react-helmet'
 
-
+const Home = _ => 
+  <div>
+    <Helmet>
+      <title>Gwenc'hlan | Potfolio</title>
+    </Helmet>
+  </div>
 
 function App() {
   return (
+    
     <div className="app">
+      <Home/>
       <body className="antialiased">
         <Menu/>
         <AboutMe/>
