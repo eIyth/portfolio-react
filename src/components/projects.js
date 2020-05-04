540ad86c1227e58d536842fc01583e7d099bde61
eIyth <gkerno@gmail.com>
2020-05-04 11:38:05 +0200

    Card layout update

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,11 +5,41 @@ class Projects extends React.Component{
     render(){
         return(
           <div className="flex p-0 w-screen items-center justify-center">
-          <div className="overflow-hidden">
+          <div className="overflow-hidden container">
               <div className="px-6 py-4">
                   <div className="text-4xl font-light text-center">Projects</div>
                   <div className=" mx-auto w-32 border-t-4 mb-10 border-indigo-400 border-collapse "></div>
                       <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+                                            <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+                                            <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+                                            <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+                                                                  <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+                                                                  <Cards 
                       name="Bot Discord" 
                       text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
                       language={['#NodeJS','#Discord API','#Fun']} 
