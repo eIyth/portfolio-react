c9ab2f24ad0d4c3191c80367341e4c1241f854fc
eIyth <gkerno@gmail.com>
2020-05-04 11:12:25 +0200

    Layout fix (mobile)

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,52 +4,27 @@ import Cards from '../components/cards.js'
 class Projects extends React.Component{
     render(){
         return(
-            <div>
-             <div className="text-3xl text-center my-5">Projects</div>
-             <div className=" flex mx-auto items-center justify-center w-1/12 border-t-4 mb-10 border-indigo-400 "></div>
-            <div className="container w-screen mt-10 mx-auto">
-            <Cards 
-              name="Location Materiel" 
-              text="Un site de location de materiel gerant la création d'utilisateur" 
-              language={['#PHP','#SQL','#BootStrap']} 
-              image='/images/LocationMateriel.png'
-              website="https://gwenchlan-le-kerneau.fr"
-            />
-            <Cards 
-              name="Bot Discord" 
-              text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
-              language={['#NodeJS','#Discord API','#Fun']} 
-              image='/images/LocationMateriel.png'
-            />
-                    <Cards 
-              name="Bot Discord" 
-              text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
-              language={['#NodeJS','#Discord API','#Fun']} 
-              image='/images/LocationMateriel.png'
-            />
-                    <Cards 
-              name="Bot Discord" 
-              text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
-              language={['#NodeJS','#Discord API','#Fun']} 
-              image='/images/LocationMateriel.png'
-            />
-                    <Cards 
-              name="Bot Discord" 
-              text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
-              language={['#NodeJS','#Discord API','#Fun']} 
-              image='/images/LocationMateriel.png'
-            />
-                    <Cards 
-              name="Bot Discord" 
-              text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
-              language={['#NodeJS','#Discord API','#Fun']} 
-              image='/images/LocationMateriel.png'
-            />
-           </div>
+          <div className="flex p-0 w-screen items-center justify-center">
+          <div className="overflow-hidden">
+              <div className="px-6 py-4">
+                  <div className="text-4xl font-light text-center ">Projects</div>
+                  <div className=" mx-auto flex w-3/12 md:w1/12 border-t-4 mb-10 border-indigo-400"></div>
+                      <Cards 
+                      name="Bot Discord" 
+                      text="Un bot Discord fait pour mon serveur personnel à la demande de mes amis" 
+                      language={['#NodeJS','#Discord API','#Fun']} 
+                      image='/images/LocationMateriel.png'
+                      />
+              </div>
+              <div className="px-6 py-4">
+              </div>
           </div>
+      </div>
             
         );
     }
 }
+
+
 
 export default Projects;
