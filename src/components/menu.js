2449e0b54bca659a753167520b243ae65d9cd02b
eIyth <gkerno@gmail.com>
2020-05-04 11:50:17 +0200

    Navbar update

--- src/components/menu.js
+++ src/components/menu.js
@@ -15,7 +15,7 @@ class Menu extends React.Component{
     <label for="menu-toggle" className="lg:hidden cursor-pointer block"><svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></label>
     <input className="hidden" type="checkbox" id="menu-toggle" />
 
-    <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+    <div className="hidden lg:flex lg:items-center lg:w-auto w-full text-right" id="menu">
         <nav>
         <ul className="lg:flex items-center justify-between text-base text-white lg:text-gray-700 pt-4 lg:pt-0">
             <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="/Home">Home</a></li>
@@ -23,9 +23,6 @@ class Menu extends React.Component{
             <li><a className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="/Contact">Contact</a></li>
         </ul>
         </nav>
-        <a href="/" className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
-        <img className="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="https://pbs.twimg.com/profile_images/1164206749093695488/e_wm8Yhc_400x400.jpg" alt="Gwenchlan Le Kerneau"/>
-        </a>
     </div>
     </header>
     </div>
