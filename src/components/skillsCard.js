3d7d74dc8d126f9823807ac5a861ed98ac073d77
Gwenc'hlan Le Kerneau <gwenchlan.le_kerneau@nokia.com>
2020-05-04 15:54:29 +0200

    Skills layout update

--- src/components/skillsCard.js
+++ src/components/skillsCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SkillsCard extends React.Component{
     render() {
       return(
-        <div className="flex items-center mx-4 h-14 my-4 border border-gray-300 rounded-lg shadow">
+        <div className="flex items-center mx-3 h-16 my-4 border border-gray-300 rounded-lg shadow">
         <img className=" ml-5 w-5 h-5 gatsby-image-wrapper" src={this.props.image} alt={this.props.name} />
         <div className="flex-1 px-2 py-4">
           <div className="font-semibold text-xs text-gray-900">{this.props.name}</div>
